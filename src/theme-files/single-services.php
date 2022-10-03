8247bd1bbee04ead1d0423ddb0df37876226272a
Aayush Rijal <arijal@aiims.com.au>
2022-10-03 20:35:55 +1100

    All done

--- src/theme-files/single-services.php
+++ src/theme-files/single-services.php
@@ -11,9 +11,14 @@ get_template_part('parts/section', 'brighte');
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-xl-11" data-aos="fade-up">
-                    <div class="heading text-secondary fw-700 fs-8 letter-spacing-n23 text-capitalize"><?= $section_1['title'] ?></div>
-                    <div class="text-light-grey line-height-2 fs-22 letter-spacing-n066 pt-3"><?= $section_1['description'] ?></div>
-
+                    <div class="row">
+                        <div class="<?= $section_1['orientation'] == 'Horizontal' ? 'col-lg-6' : 'col-12' ?>">
+                            <div class="heading text-secondary fw-700 fs-8 letter-spacing-n23 text-capitalize"><?= $section_1['title'] ?></div>
+                        </div>
+                        <div class="<?= $section_1['orientation'] == 'Horizontal' ? 'col-lg-6' : 'col-12' ?>">
+                            <div class="text-light-grey line-height-2 fs-22 letter-spacing-n066<?= $section_1['orientation'] == 'Vertical' ? ' pt-3' : '' ?>"><?= $section_1['description'] ?></div>
+                        </div>
+                    </div>
                     <div class="row justify-content-center gy-4 gy-lg-0 gx-lg-4 gx-xl-7 pt-5 pt-xl-6">
                         <?php
                         if (have_rows('section_1')) :
@@ -105,13 +110,13 @@ get_template_part('parts/section', 'brighte');
                                                         while (have_rows('template_2')) : the_row();
                                                             if (have_rows('offering_list')) :
                                                                 while (have_rows('offering_list')) : the_row();
-                                                                $title = get_sub_field('title');
-                                                                $description = get_sub_field('description');
+                                                                    $title = get_sub_field('title');
+                                                                    $description = get_sub_field('description');
                                             ?>
-                                            <div class="col-lg-6">
-                                                <div class="fw-900 fs-18"><?= $title ?></div>
-                                                <div class="description text-capitalize line-height-7 pt-3"><?= $description ?></div>
-                                            </div>
+                                                                    <div class="col-lg-6">
+                                                                        <div class="fw-900 fs-18"><?= $title ?></div>
+                                                                        <div class="description text-capitalize line-height-7 pt-3"><?= $description ?></div>
+                                                                    </div>
                                             <?php
                                                                 endwhile;
                                                             endif;
@@ -162,36 +167,39 @@ get_template_part('parts/section', 'brighte');
         </div>
     </section>
 
-    <section class="section_4">
-        <?php $section_4 = get_field('section_4'); ?>
-        <img src="<?= get_template_directory_uri() ?>/images/lib/sunpass-passport.png" alt="Sunpass Passport" class="sunpass_passport">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-xxl-11">
-                    <div class="row gy-4 gy-lg-0 gx-xl-5">
-                        <div class="col-lg-5" data-aos="fade-right">
-                            <div class="fw-700 fs-7 line-height-4 pb-3 text-capitalize"><?= $section_4['title'] ?></div>
-                            <div class="row pt-3 pt-lg-4">
-                                <?php if (isset($section_4['button_1']['url'])) : ?>
-                                    <div class="col-auto">
-                                        <a href="<?= $section_4['button_1']['url'] ?>" class="btn bordered border-white text-white px-4"><?= $section_4['button_1']['title'] ?></a>
-                                    </div>
-                                <?php endif; ?>
-                                <?php if (isset($section_4['button_2']['url'])) : ?>
-                                    <div class="col-auto">
-                                        <a href="<?= $section_4['button_2']['url'] ?>" class="btn btn-white text-blue px-4"><?= $section_4['button_2']['title'] ?></a>
-                                    </div>
-                                <?php endif; ?>
+    <?php $section_4 = get_field('section_4');
+    if (!empty($section_4['title']) && !empty($section_4['description'])) :
+    ?>
+        <section class="section_4">
+            <img src="<?= get_template_directory_uri() ?>/images/lib/sunpass-passport.png" alt="Sunpass Passport" class="sunpass_passport">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-xxl-11">
+                        <div class="row gy-4 gy-lg-0 gx-xl-5">
+                            <div class="col-lg-5" data-aos="fade-right">
+                                <div class="fw-700 fs-7 line-height-4 pb-3 text-capitalize"><?= $section_4['title'] ?></div>
+                                <div class="row pt-3 pt-lg-4">
+                                    <?php if (isset($section_4['button_1']['url'])) : ?>
+                                        <div class="col-auto">
+                                            <a href="<?= $section_4['button_1']['url'] ?>" class="btn bordered border-white text-white px-4"><?= $section_4['button_1']['title'] ?></a>
+                                        </div>
+                                    <?php endif; ?>
+                                    <?php if (isset($section_4['button_2']['url'])) : ?>
+                                        <div class="col-auto">
+                                            <a href="<?= $section_4['button_2']['url'] ?>" class="btn btn-white text-blue px-4"><?= $section_4['button_2']['title'] ?></a>
+                                        </div>
+                                    <?php endif; ?>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-lg col-xl-5 col-xxl pe-xl-5" data-aos="fade-left">
-                            <div class="description line-height-2 letter-spacing-n066"><?= $section_4['description'] ?></div>
+                            <div class="col-lg col-xl-5 col-xxl pe-xl-5" data-aos="fade-left">
+                                <div class="description line-height-2 letter-spacing-n066"><?= $section_4['description'] ?></div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+    <?php endif;    ?>
 </div>
 
 <?php get_footer(); ?>
